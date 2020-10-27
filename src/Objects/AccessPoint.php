57b311976207a1079fcce4eb97e66b5bb017d8f5
maillotf <flavien.maillot@epitech.eu>
2020-10-27 16:25:52 +0100

    Add date to Access points

--- src/Objects/AccessPoint.php
+++ src/Objects/AccessPoint.php
@@ -15,6 +15,47 @@ class AccessPoint extends AbstractArdObject
 	
 	private $description;
 	
+	/**
+	 *
+	 * @var int
+	 */
+	private $creationdate;
+	
+	/**
+	 *
+	 * @var \DateTime
+	 */
+	private $creationdatetime = null;
+	
+	/**
+	 *
+	 * @var int
+	 */
+	private $modificationdate;
+	
+	/**
+	 *
+	 * @var \DateTime
+	 */
+	private $modificationdatetime = null;		
+	
+	/**
+	 *
+	 * @var DateTimeZone
+	 */
+	private $datetimezone;
+		
+	public function __construct($datetimezone = 'Europe/Paris')
+	{
+		$this->datetimezone = new \DateTimeZone($datetimezone);
+	}
+	
+	public function init()
+	{
+		parent::init();
+		$this->setDatetimezone('Europe/Paris');
+	}
+	
 	public function setUid(int $uid): AccessPoint
 	{
 		$this->uid = $uid;
@@ -46,5 +87,60 @@ class AccessPoint extends AbstractArdObject
 	public function getDescription():string
 	{
 		return $this->description;
+	}
+	
+	public function setCreationdate($creationdate): AccessPoint
+	{
+		$this->creationdate = $creationdate;
+		return ($this);
+	}
+	
+	public function getCreationdate():int
+	{
+		return $this->creationdate;
+	}
+	
+	public function getCreationdatetime(): ?\DateTime
+	{
+		if ($this->creationdatetime == null)
+		{
+			$this->creationdatetime = new \DateTime();
+			$this->creationdatetime->setTimezone($this->datetimezone);
+			$this->creationdatetime->setTimestamp($this->getCreationdate());
+		}
+		return ($this->creationdatetime);
+	}
+	
+	public function setModificationdate($modificationdate): AccessPoint
+	{
+		$this->modificationdate = $modificationdate;
+		return ($this);
+	}
+	
+	public function getModificationdate():int
+	{
+		return $this->modificationdate;
+	}
+	
+	public function getModificationdatetime(): ?\DateTime
+	{
+		if ($this->modificationdatetime == null)
+		{
+			$this->modificationdatetime = new \DateTime();
+			$this->modificationdatetime->setTimezone($this->datetimezone);
+			$this->modificationdatetime->setTimestamp($this->getModificationdate());
+		}
+		return ($this->modificationdatetime);
+	}
+	
+	public function setDatetimezone(string $datetimezone)
+	{
+		$this->datetimezone = new \DateTimeZone($datetimezone);
+		return ($this);
+	}
+	
+	public function getDatetimezone(): \DateTimeZone
+	{
+		return ($this->datetimezone);
 	}
 }
